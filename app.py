f4fc347fd56d5e0e6a1cbf5e999702f179c9b891
brian <muvinzubrian@gmail.com>
2026-02-23 13:35:40 +0300

    added get products

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import *
 import pymysql
+import os
 
 app = Flask (__name__)
+app.config["UPLOAD_FOLDER"] = "static/images"
 
 
 @app.route("/api/signup", methods=["POST"])
@@ -59,7 +61,59 @@ def signin():
         return jsonify({"message":"log in succesful", 'user':user})
     
 
+@app.route("/api/add_product", methods= ["POST"])
+def add_products():
+    product_name = request.form["product_name"]
+    product_description = request.form["product_description"]
+    product_category = request.form["product_category"]
+    product_cost = request.form["product_cost"]
+    product_image =request.files["product_image"]
 
+  
+  
+    
+    print(product_name,product_description,product_category,product_cost,product_image)
+
+    # get image name
+    image_name = product_image.filename
+
+    # save image to folder
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'],image_name)
+    product_image.save(file_path)
+
+    connection = pymysql.connect(host='localhost',user="root",password="",database="brian_sokogarden")
+
+    cursor = connection.cursor()
+
+    # /query
+    sql = "insert into product_details (product_name,product_description,product_category,product_cost,product_image) values(%s,%s,%s,%s,%s)"
+
+    # ?data to save
+    data = (product_name,product_description,product_category,product_cost,image_name)
+    print(data)
+
+    # execute
+    cursor.execute(sql,data)
+
+    # save data
+    connection.commit()
+
+    
+    return jsonify ({"message":"product added successfully"})
+    
+@app.route("/api/get_products")
+def getproducts():
+    connection = pymysql.connect(host="localhost",user="root",password='',database="brian_sokogarden")
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    sql ="select * from product_details"
+    cursor.execute(sql)
+    if cursor.rowcount == 0:
+        return jsonify ({'message':"out of stock"})
+    else:
+        # fetch tyhe products
+        products  = cursor.fetchall()
+        return jsonify(products)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
